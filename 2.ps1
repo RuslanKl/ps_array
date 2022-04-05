4d05de7ee60e9e711f4779c1c1a67abd346f9688
My Name <you@example.com>
2022-04-05 21:16:24 +0300

    some changes

--- 2.ps1
+++ 2.ps1
@@ -4,7 +4,7 @@ foreach ($File in $FileNames) {
     $RndText = Get-Random
     $LocalPath = "E:\Temp\PS1\ScriptFiles\"+$File
     if ((Test-Path -PathType Leaf -Path $LocalPath) -ne $File) {
-    New-Item -Path "e:\Temp\PS1\ScriptFiles\" -Name "$File" -ItemType "file" -Value $RndText 
+      New-Item -Path "e:\Temp\PS1\ScriptFiles\" -Name "$File" -ItemType "file" -Value $RndText 
     }
     else {
       Write-Output ("File "+ $File +" exist")
