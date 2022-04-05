66ae0909a46566da076ba9918f02e1be34ecaae8
My Name <you@example.com>
2022-04-05 20:56:01 +0300

    Create txt files with random number with check for exist files

--- 2.ps1
+++ 2.ps1
@@ -1,3 +1,15 @@
 function ArrayFromTextFiles {
-    
+$FileNames = 'a.txt','b.txt','c.txt','d.txt','e.txt'
+foreach ($File in $FileNames) {    
+    $RndText = Get-Random
+    $LocalPath = "E:\Temp\PS1\ScriptFiles\"+$File
+    if ((Test-Path -PathType Leaf -Path $LocalPath) -ne $File) {
+    New-Item -Path "e:\Temp\PS1\ScriptFiles\" -Name "$File" -ItemType "file" -Value $RndText 
+    }
+    else {
+      Write-Output ("File "+ $File +" exist")
+    }
 }
+}
+
+ArrayFromTextFiles
